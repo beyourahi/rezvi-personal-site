604846d25b871316e541cc0013f7b73241d880ee
beyourahi <rahikhan360@gmail.com>
2022-12-18 17:03:45 +0600

    project setup️

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,13 @@
-import './globals.css'
+import { RootLayoutProps } from "types";
+import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
-}
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+    return (
+        <html lang="en">
+            <head />
+            <body>{children}</body>
+        </html>
+    );
+};
+
+export default RootLayout;
