648c00c31d73fd79cea58fa544750e9aa0442655
beyourahi <rahikhan360@gmail.com>
2023-01-05 02:12:30 +0600

    updated to Next.js 13.1

--- app/head.tsx
+++ app/head.tsx
@@ -1,4 +1,4 @@
-import { brandName } from "data.json";
+import { brandName, website_desc, prod_URL } from "data.json";
 
 const Head = () => (
     <>
@@ -7,12 +7,18 @@ const Head = () => (
         <link rel="icon" href="/favicon.ico" />
 
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="description" content="Raihana Rezvi's Personal Website" />
+        <meta name="description" content={website_desc} />
         <meta name="robots" content="index, follow" />
         <meta
             name="keywords"
             content="content writer, content, writer, raihana rezvi, raihana, rezvi, portfolio, website, personal website, portfolio website"
         />
+        <meta property="og:url" content={prod_URL} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={brandName} />
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:description" content={website_desc} />
+        <meta property="og:image" content="/preview.png" />
     </>
 );
 
